b84272f2ba5049e3c0459c6d3ead950be5d48f2c
SidKid8 <18557200+SidKid8@users.noreply.github.com>
2022-11-21 10:37:18 -0600

    Pre Sam Dev

--- ResumeCFN/functions/IncrementViewCounter.js
+++
@@ -1,9 +0,0 @@
-module.exports.handler = async (event, context) => {
-  return {
-    isBase64Encoded: false,
-    statusCode: 200,
-    headers: {},
-    multiValueHeaders: {},
-    body: ""
-  }
-}
